11207453c58bac044764d70e309973b884add730
Meschac Irung <meschacirung@gmail.com>
2024-09-24 15:58:34 +0200

    build sign-in page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { grays, palettes } from '@tailus/themer-plugins'
 
 module.exports = {
-    content: ['./index.html', './src/**/*.{html,js}'],
+    content: ['./index.html', './src/**/*.{html,js}', './pages/**/*.{html,js}'],
     darkMode: 'selector',
     theme: {
         extend: {
