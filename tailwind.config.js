37f394faa5f149d4bd6e04e626a821a7a8cc166e
Meschac Irung <meschacirung@gmail.com>
2024-09-24 15:35:17 +0200

    rebuild home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
-import { palettes } from "@tailus/themer-plugins"
+import { grays, palettes } from '@tailus/themer-plugins'
 
 module.exports = {
-    content: [
-        './index.html',
-        "./src/**/*.{html,js}"
-    ],
+    content: ['./index.html', './src/**/*.{html,js}'],
+    darkMode: 'selector',
     theme: {
         extend: {
-            colors : palettes.trust
+            colors: {
+                ...palettes.trust,
+                gray: grays.zinc,
+            },
         },
-    }
-};
+    },
+}
